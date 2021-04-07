c996ed63d1774ed52af4263a07819cf4dc3d3ada
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-04-07 18:51:27 +0900

    Cardコンポーネントの作成

--- src/store/index.js
+++ src/store/index.js
@@ -31,10 +31,28 @@ const store = new Vuex.Store({
     addlist(state, payload){
       state.lists.push({title: payload.title, cards: []})
     },
+    removelist(state, payload) {
+      state.lists.splice(payload.listIndex, 1)
+    },
+    addCardToList(state,payload){
+      state.lists[payload.listIndex].cards.push({body:payload.body})
+    },
+    removeCardFromList(state, payload) {
+      state.lists[payload.listIndex].cards.splice(payload.cardIndex, 1)
+    },
   },
   actions: {
     addlist(context, payload){
       context.commit('addlist', payload)
+    },
+    removelist(context, payload) {
+      context.commit('removelist', payload)
+    },
+    addCardToList(context,payload){
+      context.commit('addCardToList',payload)
+    },
+    removeCardFromList(context, payload) {
+      context.commit('removeCardFromList', payload)
     },
   },
   getters: {
