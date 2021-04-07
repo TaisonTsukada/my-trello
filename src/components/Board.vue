760b885d1f2cab55d3f028d2be7cbf5119ca9167
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-04-07 19:29:01 +0900

    リストもドラッグできるようにした

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,7 +5,9 @@
     </header>
     <main>
       <p class="info-line">All: {{totalCardCount}}</p>
-      <div class="list-index">
+      <draggable :list="lists" 
+      @end = "movingList"
+      class="list-index">
         <list v-for="(item, index) in lists"
               :key="item.id"
               :title="item.title"
@@ -14,19 +16,21 @@
               @change="movingCard"
         />
         <list-add />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import ListAdd from './ListAdd.vue'
 import List from './List'
 import { mapState } from 'vuex'
 export default {
   components: {
     ListAdd,
-    List
+    List,
+    draggable
   },
   computed: {
       ...mapState([
@@ -38,6 +42,9 @@ export default {
     },
     methods:{
       movingCard: function(){
+        this.$store.dispatch('updateList',{lists: this.lists})
+      },
+      movingList: function(){
         this.$store.dispatch('updateList',{lists: this.lists})
       }
     }
